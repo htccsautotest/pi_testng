6e886dd82a1486679dde4f08e974985c2be59e51
guile_chao <guile_chao@htc.com>
2012-03-13 09:57:24 +0800

    add listener

--- src/test/java/Module1Test.java
+++ src/test/java/Module1Test.java
@@ -1,20 +1,30 @@
 import static org.testng.AssertJUnit.*;
 import org.testng.annotations.Test;
+import org.testng.annotations.Listeners;
 
-
+@Listeners({ Listener.class })
 public class Module1Test {
-	@Test(groups = { "P1", "FVT" })
-	public void testMethod1() {
+	@Test(description = "this is test1 description"
+			, enabled = true			
+			, timeOut = 30 * 1000
+			, groups = { "P0", "BVT" })
+	public void TC00001() {
 		assertEquals(1, 1);
 	}
 
-	@Test(groups = { "P2", "BVT" })
-	public void testMethod2() {
-		assertEquals(1, 1);
+	@Test(description = "this is test2 description"
+			, enabled = true			
+			, timeOut = 30 * 1000
+			, groups = { "P1", "FVT" })
+	public void TC00002() {
+		assertEquals(1, 2);
 	}
 	
-	@Test(groups = { "P1", "FVT" })
-	public void testMethod3() {
+	@Test(description = "this is test3 description"
+			, enabled = true			
+			, timeOut = 30 * 1000
+			, groups = { "P2", "BVT" })
+	public void TC00003() {
 		assertEquals(1, 1);
 	}
 }
